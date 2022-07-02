79f1c102f30b48479629c0e1354d3663db0be382
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-02 13:49:08 +0530

    Employee daily wage calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,12 +3,17 @@ public class EmployeeWage
 public static void main(String[] args)
 {
 System.out.println("Welcome to Employee wage computation program");
-final int isPresent=1;
+final int ISPRESENT=1;
+final int WAGE_PER_HOUR=20;
+final int FULL_DAY_HOUR=8;
+int dailyWage=0;
 int empStatus = (int)(Math.random()*2);
 System.out.println(empStatus);
-if(empStatus==isPresent)
+if(empStatus==ISPRESENT)
 {
 	System.out.println("Employee is present");
+	dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+	System.out.println("Daily Wage of Employee is "+dailyWage);
 }
 else
 {
