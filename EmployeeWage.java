5082fa0a8a5800bb602857902903193dd6415646
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-02 15:39:34 +0530

    Condition of maximum hours and maximum days checked

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,10 +10,12 @@ final int ABSENT=0;
 final int FULL_TIME=1;
 final int PART_TIME=2;
 final int WORKING_DAYS_PER_MONTH=20;
+final int TOTAL_WORKING_HOURS=100;
 int dailyWage=0;
 int monthlyWages=0;
 int days=1;
-while(days<=WORKING_DAYS_PER_MONTH)
+int totalHours=0;
+while(days<=WORKING_DAYS_PER_MONTH && totalHours<=TOTAL_WORKING_HOURS)
 {
 int empStatus = (int)(Math.random()*3);
 System.out.println(empStatus);
@@ -23,12 +25,14 @@ switch(empStatus)
 	System.out.println("Employee is present Full time");
 	dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
 	monthlyWages=monthlyWages+dailyWage;
+	totalHours=totalHours+FULL_DAY_HOUR;
 	System.out.println("Daily Wage of Employee is "+dailyWage);
 	break;
 	case PART_TIME:
 	System.out.println("Employee is present Half time");
 	dailyWage=WAGE_PER_HOUR*HALF_DAY_HOUR;
 	monthlyWages=monthlyWages+dailyWage;
+	totalHours=totalHours+HALF_DAY_HOUR;
 	System.out.println("Daily Wage of Employee is "+dailyWage);
 	break;
 	case ABSENT:
@@ -41,5 +45,6 @@ System.out.println("Day number "+days);
 days++;
 }
 System.out.println("Monthly wages of Employee "+monthlyWages);
+System.out.println("Total working hours of Employee "+totalHours);
 }
 }
